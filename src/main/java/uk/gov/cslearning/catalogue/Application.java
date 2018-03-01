14f39441f1ea1443e0c8a3d4cef701ae822d3b56
Max Stewart <max@cautionyourblast.com>
2018-03-01 17:27:54 +0000

    Added BASIC auth to all endpoints.

--- src/main/java/uk/gov/cslearning/catalogue/Application.java
+++ src/main/java/uk/gov/cslearning/catalogue/Application.java
@@ -2,15 +2,11 @@ package uk.gov.cslearning.catalogue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import uk.gov.cslearning.catalogue.config.DefaultConfig;
 
 /**
  * Main Spring application configuration and entry point.
  */
 @SpringBootApplication
-@Import(DefaultConfig.class)
 public class Application {
 
     public static void main(String[] args) {
