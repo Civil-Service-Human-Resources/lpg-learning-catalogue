cd430ae11311084433d31b8e13c96755e7cc1362
matthewmu <matthewmu@kainos.com>
2018-03-21 12:04:10 +0000

    refactoring

--- src/test/java/uk/gov/cslearning/catalogue/repository/CourseSearchRepositoryIT.java
+++ src/test/java/uk/gov/cslearning/catalogue/repository/CourseSearchRepositoryIT.java
@@ -4,9 +4,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.cslearning.catalogue.domain.Course;
+import uk.gov.cslearning.catalogue.domain.SearchPage;
 
 import java.util.List;
 
@@ -25,12 +27,27 @@ public class CourseSearchRepositoryIT {
     @Autowired
     private CourseRepository repository;
 
-
     @Test
-    public void tester(){
+    public void testMisspelledSearchQueryReturnsAccurateSuggestionAndCourse() {
+        SearchPage actualSearchPage = repository.search("Wirking with Budgets");
 
-        List<Course> actualCourseList = repository.suggestions("This text");
-        assertEquals(actualCourseList.get(0).getTitle(), "Test");
+        String actualSuggestionText = actualSearchPage.getTopScoringSuggestion().getText().toString();
+        Page<Course> coursePage = actualSearchPage.getCourses();
+        List<Course> courseList = coursePage.getContent();
+        String actualTitle = courseList.get(0).getTitle();
+
+        assertEquals("working with budgets", actualSuggestionText);
+        assertEquals("Working with budgets", actualTitle);
     }
 
+    @Test
+    public void testSearchQueryReturnsCorrectCoursePage() {
+        SearchPage actualSearchPage = repository.search("Budgets");
+        List<Course> actualCourses = actualSearchPage.getCourses().getContent();
+
+        assertEquals(2, actualCourses.size());
+        assertEquals("Working with budgets", actualCourses.get(0).getTitle());
+        assertEquals("BUfZwRaWQrKAhSSjlJ7lCg", actualCourses.get(0).getId());
+        assertEquals("This topic introduces you to the fundamental principles of budget management and governance processes. ", actualCourses.get(0).getShortDescription());
+    }
 }
