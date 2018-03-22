df677d7aee633fb97940893d2dd82cb17a9d6bfe
matthewmu <matthewmu@kainos.com>
2018-03-22 16:50:05 +0000

    adding jest config

--- src/main/java/uk/gov/cslearning/catalogue/repository/CourseSearchRepositoryImpl.java
+++ src/main/java/uk/gov/cslearning/catalogue/repository/CourseSearchRepositoryImpl.java
@@ -58,6 +58,7 @@ public class CourseSearchRepositoryImpl implements CourseSearchRepository {
                 .withPageable(pageable)
                 .build();
 
-        return template.queryForPage(searchQuery, Course.class);
+        return operations.queryForPage(searchQuery, Course.class);
+
     }
 }
