62ceaecdc823fbb53129606d41c325f2d85ddc47
matthewmu <matthewmu@kainos.com>
2018-03-23 14:07:24 +0000

    PR fixes

--- src/main/java/uk/gov/cslearning/catalogue/config/ElasticSearchProperties.java
+++ src/main/java/uk/gov/cslearning/catalogue/config/ElasticSearchProperties.java
@@ -3,42 +3,24 @@ package uk.gov.cslearning.catalogue.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import java.net.URI;
+
 @Component
 @ConfigurationProperties(prefix = "elasticsearch")
 public class ElasticSearchProperties {
 
-    private String clusterName;
-
-    private String host;
-
-    private int port;
+    private URI uri;
 
     private String username;
 
     private String password;
 
-    public String getClusterName() {
-        return clusterName;
+    public URI getUri() {
+        return uri;
     }
 
-    public void setClusterName(String clusterName) {
-        this.clusterName = clusterName;
-    }
-
-    public String getHost() {
-        return host;
-    }
-
-    public void setHost(String host) {
-        this.host = host;
-    }
-
-    public int getPort() {
-        return port;
-    }
-
-    public void setPort(int port) {
-        this.port = port;
+    public void setUri(URI uri) {
+        this.uri = uri;
     }
 
     public String getUsername() {
