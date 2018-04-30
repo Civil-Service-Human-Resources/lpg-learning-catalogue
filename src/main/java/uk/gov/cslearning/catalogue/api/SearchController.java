dd2fba763dd6ef42a6d362d95e886126e25a5840
alan <alan.work@teamsmog.com>
2018-04-30 18:48:35 +0100

    course details for module
    load data
    
    load data
    
    load data

--- src/main/java/uk/gov/cslearning/catalogue/api/SearchController.java
+++ src/main/java/uk/gov/cslearning/catalogue/api/SearchController.java
@@ -4,14 +4,22 @@ package uk.gov.cslearning.catalogue.api;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import uk.gov.cslearning.catalogue.domain.Course;
 import uk.gov.cslearning.catalogue.domain.Resource;
 import uk.gov.cslearning.catalogue.domain.SearchPage;
+import uk.gov.cslearning.catalogue.repository.CourseRepository;
 import uk.gov.cslearning.catalogue.repository.ResourceRepository;
+
+import java.util.ArrayList;
+
 
 import static com.google.common.base.Preconditions.checkArgument;
 
@@ -22,11 +30,13 @@ public class SearchController {
     private static final Logger LOGGER = LoggerFactory.getLogger(SearchController.class);
 
     private ResourceRepository resourceRepository;
+    private CourseRepository courseRepository;
 
     @Autowired
-    public SearchController(ResourceRepository resourceRepository) {
+    public SearchController(ResourceRepository resourceRepository,CourseRepository courseRepository) {
         checkArgument(resourceRepository != null);
         this.resourceRepository = resourceRepository;
+        this.courseRepository = courseRepository;
     }
 
     @GetMapping
@@ -39,5 +49,25 @@ public class SearchController {
         SearchResults<Resource> searchResults = new SearchResults<>(searchPage, pageable);
 
         return ResponseEntity.ok(searchResults);
+    }
+
+
+    @GetMapping("/create")
+    public ResponseEntity  create() {
+
+        Pageable pageable = PageRequest.of(0, (int) courseRepository.count());
+
+        Page<Course> page;
+
+        LOGGER.debug("Creating search indexes");
+        Iterable<Course> courses = courseRepository.findAll(pageable);
+        for (Course course: courses) {
+            ArrayList<Resource> resources = Resource.fromCourse(course);
+            for (Resource resource : resources) {
+                LOGGER.debug("Creating resource {}", resource);
+                resourceRepository.save(resource);
+            }
+        }
+        return ResponseEntity.noContent().build();
     }
 }
