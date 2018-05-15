9d7f128d795a1b6b89bca9557dd87d42a86e58dd
Jennifer Duong <jen@cautionyourblast.com>
2018-05-15 14:35:26 +0100

    LPFG-338

---
+++ src/main/java/uk/gov/cslearning/catalogue/domain/module/FileModule.java
@@ -0,0 +1,24 @@
+package uk.gov.cslearning.catalogue.domain.module;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
+@JsonTypeName("file")
+public class FileModule extends Module {
+
+    private String url;
+
+    @JsonCreator
+    public FileModule(@JsonProperty("url") String url) {
+        setUrl(url);
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
