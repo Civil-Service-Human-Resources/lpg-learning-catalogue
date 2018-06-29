a6373a7a2808a1b10f7159631ef3b9ef37f0a144
Max Stewart <max@cautionyourblast.com>
2018-06-29 10:29:30 +0100

    Add purchase order domain and API

---
+++ src/main/java/uk/gov/cslearning/catalogue/domain/PurchaseOrder.java
@@ -0,0 +1,86 @@
+package uk.gov.cslearning.catalogue.domain;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import org.elasticsearch.common.UUIDs;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashSet;
+
+@Document(indexName = "lpg-payments", type = "purchaseOrder")
+public class PurchaseOrder {
+
+    @Id
+    private String id = UUIDs.randomBase64UUID();
+
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    private LocalDate validFrom;
+
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    private LocalDate validTo;
+
+    private BigDecimal amount;
+
+    private String department;
+
+    private Collection<String> modules = new HashSet<>();
+
+    public PurchaseOrder() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public LocalDate getValidFrom() {
+        return validFrom;
+    }
+
+    public void setValidFrom(LocalDate validFrom) {
+        this.validFrom = validFrom;
+    }
+
+    public LocalDate getValidTo() {
+        return validTo;
+    }
+
+    public void setValidTo(LocalDate validTo) {
+        this.validTo = validTo;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public Collection<String> getModules() {
+        return modules;
+    }
+
+    public void setModules(Collection<String> modules) {
+        this.modules = modules;
+    }
+}
