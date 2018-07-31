b249da99a0acdda59b45f8a3ca4df9c0604f1832
Rob Leney <roble@kainos.com>
2018-07-31 15:09:53 +0100

    LPFG-570: Added exception handler.

---
+++ src/main/java/uk/gov/cslearning/catalogue/api/ApiExceptionHandler.java
@@ -0,0 +1,15 @@
+package uk.gov.cslearning.catalogue.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class ApiExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalStateException.class)
+    public void handleIllegalState() {
+    }
+}
