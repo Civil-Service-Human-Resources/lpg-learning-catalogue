6728c79ff3d91309c8de18fd225905f373c3e55d
matthewmu <matthewmu@kainos.com>
2018-08-02 16:43:10 +0100

    LPFG-520: Initial learning provider, cancellation policy and t and c

--- src/test/java/uk/gov/cslearning/catalogue/api/TermsAndConditionsControllerTest.java
+++ src/test/java/uk/gov/cslearning/catalogue/api/TermsAndConditionsControllerTest.java
@@ -7,6 +7,10 @@ import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -14,13 +18,17 @@ import org.springframework.test.web.servlet.MockMvc;
 import uk.gov.cslearning.catalogue.domain.TermsAndConditions;
 import uk.gov.cslearning.catalogue.repository.TermsAndConditionsRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebMvcTest(TermsAndConditionsController.class)
@@ -28,8 +36,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class TermsAndConditionsControllerTest {
 
     public static final String ID = "abc123";
-    public static final String NAME = "New Terms";
-    public static final String DESCRIPTION = "New description";
+    public static final String NAME = "New Terms and Conditions";
+    public static final String DESCRIPTION = "Example description";
     public static final String EMAIL = "test@example.com";
 
     @Autowired
@@ -40,19 +48,23 @@ public class TermsAndConditionsControllerTest {
 
     private Gson gson = new Gson();
 
+    private TermsAndConditions createTermsAndConditions() {
+        return new TermsAndConditions(NAME, DESCRIPTION, EMAIL);
+    }
+
     @Test
-    public void shouldCreateLearningProviderAndRedirectToNewResource() throws Exception {
-        TermsAndConditions termsAndConditions = createTermsAndConditions();
+    public void shouldCreateTermsAndConditionsAndRedirectToNewPolicy() throws Exception {
+        TermsAndConditions TermsAndConditions = createTermsAndConditions();
 
         when(termsAndConditionsRepository.save(any()))
                 .thenAnswer((Answer<TermsAndConditions>) invocation -> {
-                    termsAndConditions.setId(ID);
-                    return termsAndConditions;
+                    TermsAndConditions.setId(ID);
+                    return TermsAndConditions;
                 });
 
         mockMvc.perform(
                 post("/terms-and-conditions").with(csrf())
-                        .content(gson.toJson(termsAndConditions))
+                        .content(gson.toJson(TermsAndConditions))
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
@@ -60,7 +72,105 @@ public class TermsAndConditionsControllerTest {
                 .andExpect(header().string("location", "http://localhost/terms-and-conditions/" + ID));
     }
 
-    private TermsAndConditions createTermsAndConditions() {
-        return new TermsAndConditions(NAME, DESCRIPTION, EMAIL);
+    @Test
+    public void shouldGetTermsAndConditions() throws Exception {
+        List<String> ids = new ArrayList<>();
+        ids.add(ID);
+
+        TermsAndConditions TermsAndConditions = createTermsAndConditions();
+
+        when(termsAndConditionsRepository.findById(ID))
+                .thenReturn(Optional.of(TermsAndConditions));
+
+        mockMvc.perform(
+                get("/terms-and-conditions/" + ID)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(NAME)))
+                .andExpect(jsonPath("$.createdByEmail", is(EMAIL)))
+                .andExpect(jsonPath("$.description", is(DESCRIPTION)));
     }
+
+    @Test
+    public void shouldUpdateTermsAndConditions() throws Exception {
+        TermsAndConditions TermsAndConditions = createTermsAndConditions();
+        when(termsAndConditionsRepository.existsById(TermsAndConditions.getId())).thenReturn(true);
+        when(termsAndConditionsRepository.save(any())).thenReturn(TermsAndConditions);
+
+        mockMvc.perform(
+                put("/terms-and-conditions/" + TermsAndConditions.getId()).with(csrf())
+                        .content(gson.toJson(TermsAndConditions))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    public void shouldSendBadRequestIfTermsAndConditionsDoesntExistWhenUpdating() throws Exception {
+        TermsAndConditions TermsAndConditions = createTermsAndConditions();
+        when(termsAndConditionsRepository.existsById(TermsAndConditions.getId())).thenReturn(false);
+
+        mockMvc.perform(
+                put("/terms-and-conditions/" + TermsAndConditions.getId()).with(csrf())
+                        .content(gson.toJson(TermsAndConditions))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void shouldDeleteTermsAndConditions() throws Exception {
+        TermsAndConditions TermsAndConditions = createTermsAndConditions();
+        when(termsAndConditionsRepository.existsById(TermsAndConditions.getId())).thenReturn(true);
+
+        mockMvc.perform(
+                post("/terms-and-conditions/" + TermsAndConditions.getId()).with(csrf())
+                        .content(gson.toJson(TermsAndConditions))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    public void shouldSendBadRequestIfTermsAndConditionsDoesntExistWhenDeleting() throws Exception {
+        TermsAndConditions TermsAndConditions = createTermsAndConditions();
+        when(termsAndConditionsRepository.existsById(TermsAndConditions.getId())).thenReturn(false);
+
+        mockMvc.perform(
+                post("/terms-and-conditions/" + TermsAndConditions.getId()).with(csrf())
+                        .content(gson.toJson(TermsAndConditions))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void shouldListCancellationPolicies() throws Exception {
+        Pageable pageable = PageRequest.of(0, 10);
+
+        List<TermsAndConditions> termsAndConditionsList = new ArrayList<>();
+        termsAndConditionsList.add(new TermsAndConditions(NAME + "1", DESCRIPTION, EMAIL));
+        termsAndConditionsList.add(new TermsAndConditions(NAME + "2", DESCRIPTION, EMAIL));
+
+        Page<TermsAndConditions> cancellationPolicies = new PageImpl<>(termsAndConditionsList);
+
+        when(termsAndConditionsRepository.findAll(pageable))
+                .thenReturn(cancellationPolicies);
+
+        mockMvc.perform(
+                get("/terms-and-conditions")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.results", hasSize(2)))
+                .andExpect(jsonPath("$.results[*].name", containsInAnyOrder(NAME + "1", NAME + "2")))
+                .andExpect(jsonPath("$.results[0].description", is("Example description")))
+                .andExpect(jsonPath("$.results[0].createdByEmail", is("test@example.com")));
+    }
+
 }
