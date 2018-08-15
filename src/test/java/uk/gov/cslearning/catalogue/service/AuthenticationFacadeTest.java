3887aa8c76ebde0ed77c1dba3b5355caba848997
Rob Leney <roble@kainos.com>
2018-08-15 10:41:19 +0100

    LPFG-663: Added tests for AuditableLearningProviderRepository and AuthenticationFacade.

---
+++ src/test/java/uk/gov/cslearning/catalogue/service/AuthenticationFacadeTest.java
@@ -0,0 +1,23 @@
+package uk.gov.cslearning.catalogue.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@WithMockUser(username = "test-user", password = "password")
+public class AuthenticationFacadeTest {
+    @Autowired
+    private AuthenticationFacade authenticationFacade;
+
+    @Test
+    public void getAuthenticationReturnsAuthentication() {
+        assertEquals("test-user", authenticationFacade.getAuthentication().getName());
+    }
+}
