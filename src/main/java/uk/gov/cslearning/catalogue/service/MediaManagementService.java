cb3c6be2775b0a9b783e8a70b8256906853ca48c
Rob Leney <roble@kainos.com>
2018-08-20 15:54:09 +0100

    LPFG-569: Added MediaRepository and domain classes. Save media to ElasticSearch after uploading

--- src/main/java/uk/gov/cslearning/catalogue/service/MediaManagementService.java
+++ src/main/java/uk/gov/cslearning/catalogue/service/MediaManagementService.java
@@ -1,7 +1,7 @@
 package uk.gov.cslearning.catalogue.service;
 
+import uk.gov.cslearning.catalogue.domain.media.Media;
 import uk.gov.cslearning.catalogue.dto.FileUpload;
-import uk.gov.cslearning.catalogue.dto.Media;
 
 public interface MediaManagementService {
     Media create(FileUpload fileUpload);
