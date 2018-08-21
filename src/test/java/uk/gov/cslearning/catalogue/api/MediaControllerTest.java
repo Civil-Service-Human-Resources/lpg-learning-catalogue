e096831a83a7b5aec443378c740506f5697509d2
Rob Leney <roble@kainos.com>
2018-08-21 14:36:56 +0100

    LPFG-569: Changed successful response status from 204 to 201

--- src/test/java/uk/gov/cslearning/catalogue/api/MediaControllerTest.java
+++ src/test/java/uk/gov/cslearning/catalogue/api/MediaControllerTest.java
@@ -10,11 +10,16 @@ import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import uk.gov.cslearning.catalogue.domain.media.Media;
+import uk.gov.cslearning.catalogue.dto.FileUpload;
 import uk.gov.cslearning.catalogue.service.FileUploadFactory;
 import uk.gov.cslearning.catalogue.service.MediaManagementService;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -33,14 +38,25 @@ public class MediaControllerTest {
 
     @Test
     public void shouldUploadFileOnPostRequest() throws Exception {
-        MockMultipartFile file = new MockMultipartFile("test.doc", "".getBytes());
+        String fileContainer = "container-id";
+        String mediaUid = "media-uid";
+
+        MockMultipartFile file = new MockMultipartFile("file", "file.doc", "application/octet-stream", "abc".getBytes());
+        FileUpload fileUpload = mock(FileUpload.class);
+
+        when(fileUploadFactory.create(file, fileContainer)).thenReturn(fileUpload);
+
+        Media media = mock(Media.class);
+        when(media.getUid()).thenReturn(mediaUid);
+        when(mediaManagementService.create(fileUpload)).thenReturn(media);
 
         mockMvc.perform(
                 multipart("/service/media")
                         .file(file)
-                        .param("container", "container-id")
-                        .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                        .param("container", fileContainer)
+                        .contentType(MediaType.MULTIPART_FORM_DATA)
                         .accept(MediaType.APPLICATION_JSON).with(csrf()))
-                .andExpect(status().isNoContent());
+                .andExpect(status().isCreated())
+                .andExpect(header().string("location", "http://localhost/service/media/" + mediaUid));
     }
 }
