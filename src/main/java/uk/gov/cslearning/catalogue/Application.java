8e85fea4908af963fc8a9753d3f5ab49fccfd90f
Rob Leney <roble@kainos.com>
2018-08-21 14:33:44 +0100

    LPFG-569: Moved Azure config to separate config class to allow controllers to be tested without loading the entire stack.

--- src/main/java/uk/gov/cslearning/catalogue/Application.java
+++ src/main/java/uk/gov/cslearning/catalogue/Application.java
@@ -1,30 +1,15 @@
 package uk.gov.cslearning.catalogue;
 
-import com.microsoft.azure.storage.CloudStorageAccount;
-import com.microsoft.azure.storage.blob.CloudBlobClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-
-import java.net.URISyntaxException;
-import java.security.InvalidKeyException;
 
 /**
  * Main Spring application configuration and entry point.
  */
 @SpringBootApplication(exclude = {ElasticsearchAutoConfiguration.class, ElasticsearchDataAutoConfiguration.class})
 public class Application {
-
-    @Autowired
-    private CloudStorageAccount cloudStorageAccount;
-
-    @Bean
-    public CloudBlobClient storageClient() throws URISyntaxException, InvalidKeyException {
-        return cloudStorageAccount.createCloudBlobClient();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
