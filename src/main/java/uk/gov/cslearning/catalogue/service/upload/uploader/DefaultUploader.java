baa2fdd4b540d328b310df99b63bb2ae916ea343
Rob Leney <roble@kainos.com>
2018-08-29 12:37:12 +0100

    LPFG-569: Added test for DefaultUploader

--- src/main/java/uk/gov/cslearning/catalogue/service/upload/uploader/DefaultUploader.java
+++ src/main/java/uk/gov/cslearning/catalogue/service/upload/uploader/DefaultUploader.java
@@ -1,27 +1,40 @@
 package uk.gov.cslearning.catalogue.service.upload.uploader;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import uk.gov.cslearning.catalogue.dto.ProcessedFile;
 import uk.gov.cslearning.catalogue.dto.Upload;
 import uk.gov.cslearning.catalogue.dto.UploadedFile;
-import uk.gov.cslearning.catalogue.exception.FileUploadException;
 import uk.gov.cslearning.catalogue.service.upload.client.UploadClient;
 
 import java.io.IOException;
+import java.util.Collections;
 
 @Component
 public class DefaultUploader implements Uploader {
+    private static final Logger LOG = LoggerFactory.getLogger(DefaultUploader.class);
+    private final UploadFactory uploadFactory;
+
+    public DefaultUploader(UploadFactory uploadFactory) {
+        this.uploadFactory = uploadFactory;
+    }
 
     @Override
     public Upload upload(ProcessedFile processedFile, UploadClient uploadClient) {
         String filePath = String.join("/", processedFile.getFileUpload().getContainer(), processedFile.getFileUpload().getId());
 
         try {
-            UploadedFile uploadedFile = uploadClient.upload(processedFile.getFileUpload().getFile().getInputStream(), filePath, processedFile.getFileUpload().getFile().getSize());
-            return new Upload();
+            UploadedFile uploadedFile = uploadClient.upload(processedFile.getFileUpload().getFile().getInputStream(),
+                    filePath, processedFile.getFileUpload().getFile().getSize());
+
+            return uploadFactory.successfulUpload(processedFile, Collections.singletonList(uploadedFile), filePath);
 
         } catch (IOException e) {
-            throw new FileUploadException(e);
+            LOG.error("Upload failed", e);
+            return uploadFactory.failedUpload(processedFile, filePath, e);
+
+
         }
     }
 }
