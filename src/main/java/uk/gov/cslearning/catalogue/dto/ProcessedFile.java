205fdbd192aa481a2a4b0287160a8cd00a27cb17
Rob Leney <roble@kainos.com>
2018-08-30 15:09:00 +0100

    LPFG-569: Added metadata to ProcessedFile and Media

--- src/main/java/uk/gov/cslearning/catalogue/dto/ProcessedFile.java
+++ src/main/java/uk/gov/cslearning/catalogue/dto/ProcessedFile.java
@@ -1,5 +1,24 @@
 package uk.gov.cslearning.catalogue.dto;
 
-public interface ProcessedFile {
-    FileUpload getFileUpload();
+import java.util.Map;
+
+public class ProcessedFile {
+    private FileUpload fileUpload;
+    private Map<String, String> metadata;
+
+    public FileUpload getFileUpload() {
+        return fileUpload;
+    }
+
+    public void setFileUpload(FileUpload fileUpload) {
+        this.fileUpload = fileUpload;
+    }
+
+    public Map<String, String> getMetadata() {
+        return metadata;
+    }
+
+    public void setMetadata(Map<String, String> metadata) {
+        this.metadata = metadata;
+    }
 }
