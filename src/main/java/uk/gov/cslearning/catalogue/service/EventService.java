0e535c89d3a20e587bb53115ecbbb0e4bec125c9
Peter Taylor <petertaylor@Malekith.local>
2018-08-31 16:57:56 +0100

    LPFG-576: Added events

--- src/main/java/uk/gov/cslearning/catalogue/service/EventService.java
+++ src/main/java/uk/gov/cslearning/catalogue/service/EventService.java
@@ -3,10 +3,12 @@ package uk.gov.cslearning.catalogue.service;
 import org.springframework.stereotype.Service;
 import uk.gov.cslearning.catalogue.domain.Course;
 import uk.gov.cslearning.catalogue.domain.module.Event;
-import uk.gov.cslearning.catalogue.domain.module.Module;
+import uk.gov.cslearning.catalogue.domain.module.FaceToFaceModule;
 import uk.gov.cslearning.catalogue.repository.CourseRepository;
 
-import java.util.ArrayList;
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Optional;
 import java.util.function.Supplier;
 
@@ -24,11 +26,23 @@ public class EventService {
                     String.format("Unable to add module. Course does not exist: %s", courseId));
         });
 
-        Module module = course.getModuleById(moduleId);
+        FaceToFaceModule module = (FaceToFaceModule) course.getModuleById(moduleId);
 
-        ArrayList<Event> events = module.getEvents();
-        events.add(event);
-        module.setEvents(events);
+        Collection<Event> events = module.getEvents();
+
+        HashSet<Event> newEvents = new HashSet<>();
+
+        for(Event e : events){
+            newEvents.add(e);
+        }
+
+        newEvents.add(event);
+
+        System.out.println(event.getDateRanges().get(0).getDate());
+        System.out.println(event.getDateRanges().get(0).getStartTime());
+        System.out.println(event.getDateRanges().get(0).getEndTime());
+
+        module.setEvents(newEvents);
         courseRepository.save(course);
 
         return event;
@@ -40,7 +54,7 @@ public class EventService {
                     String.format("Unable to find module: %s. Course does not exist: %s", moduleId, courseId));
         });
 
-        Module module = course.getModuleById(moduleId);
+        FaceToFaceModule module = (FaceToFaceModule) course.getModuleById(moduleId);
 
         return module.getEvents().stream().filter(e -> e.getId().equals(eventId)).findFirst();
     }
