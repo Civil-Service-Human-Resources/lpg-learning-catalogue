d4b08cc01115cb046aeef10eab014bdb53e4cf53
Rob Leney <roble@kainos.com>
2018-09-03 13:22:15 +0100

    LPFG-569: Subsitute files in elearning packages

--- src/main/java/uk/gov/cslearning/catalogue/service/upload/uploader/ScormUploader.java
+++ src/main/java/uk/gov/cslearning/catalogue/service/upload/uploader/ScormUploader.java
@@ -2,28 +2,38 @@ package uk.gov.cslearning.catalogue.service.upload.uploader;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import uk.gov.cslearning.catalogue.dto.ProcessedFile;
 import uk.gov.cslearning.catalogue.dto.Upload;
 import uk.gov.cslearning.catalogue.dto.UploadedFile;
-import uk.gov.cslearning.catalogue.service.upload.ZipInputStreamFactory;
+import uk.gov.cslearning.catalogue.service.upload.FileFactory;
+import uk.gov.cslearning.catalogue.service.upload.InputStreamFactory;
 import uk.gov.cslearning.catalogue.service.upload.client.UploadClient;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 @Component
 public class ScormUploader implements Uploader {
     private static final Logger LOG = LoggerFactory.getLogger(ScormUploader.class);
-    private final ZipInputStreamFactory zipInputStreamFactory;
+    private final InputStreamFactory inputStreamFactory;
     private final UploadFactory uploadFactory;
+    private FileFactory fileFactory;
+    private final Map<String, String> fileSubstitutions;
 
-    public ScormUploader(ZipInputStreamFactory zipInputStreamFactory, UploadFactory uploadFactory) {
-        this.zipInputStreamFactory = zipInputStreamFactory;
+    public ScormUploader(InputStreamFactory inputStreamFactory, UploadFactory uploadFactory, FileFactory fileFactory, @Qualifier("fileSubstitions") Map<String, String> fileSubstitutions) {
+        this.inputStreamFactory = inputStreamFactory;
         this.uploadFactory = uploadFactory;
+        this.fileFactory = fileFactory;
+        this.fileSubstitutions = fileSubstitutions;
     }
 
     @Override
@@ -32,20 +42,27 @@ public class ScormUploader implements Uploader {
 
         String destinationDirectory = String.join("/", processedFile.getFileUpload().getContainer(), processedFile.getFileUpload().getId());
 
-        try (ZipInputStream inputStream = zipInputStreamFactory.create(processedFile.getFileUpload().getFile().getInputStream())) {
+        try (ZipInputStream inputStream = inputStreamFactory.createZipInputStream(processedFile.getFileUpload().getFile().getInputStream())) {
             ZipEntry zipEntry = inputStream.getNextEntry();
-
 
             while (zipEntry != null) {
                 String filePath = String.join("/", destinationDirectory, zipEntry.getName());
 
-                long length;
-                long size = 0;
-                while ((length = inputStream.read(new byte[1024])) > 0) {
-                    size += length;
+                if (fileSubstitutions.containsKey(zipEntry.getName())) {
+                    File file = fileFactory.get(fileSubstitutions.get(zipEntry.getName()));
+                    try (InputStream fileInputStream = inputStreamFactory.createFileInputStream(file)) {
+                        uploadedFiles.add(uploadClient.upload(fileInputStream, filePath, file.length()));
+                    }
                 }
+                else {
+                    long length;
+                    long size = 0;
+                    while ((length = inputStream.read(new byte[1024])) > 0) {
+                        size += length;
+                    }
 
-                uploadedFiles.add(uploadClient.upload(inputStream, filePath, size));
+                    uploadedFiles.add(uploadClient.upload(inputStream, filePath, size));
+                }
 
                 zipEntry = inputStream.getNextEntry();
             }
@@ -53,7 +70,7 @@ public class ScormUploader implements Uploader {
             inputStream.closeEntry();
             return uploadFactory.createUpload(processedFile, uploadedFiles, destinationDirectory);
 
-        } catch (IOException e) {
+        } catch (URISyntaxException | IOException e) {
             LOG.error(String.format("Unable to upload Scorm package: %s", processedFile), e);
             return uploadFactory.createFailedUpload(processedFile, destinationDirectory, e);
         }
