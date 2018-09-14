a5ae2a2b480483e839a8d5d38e08ca8b975ce39d
Rob Leney <roble@kainos.com>
2018-09-14 15:09:52 +0100

    LPFG-766: Added MetadataParser class to abstract out metadata parsing.

--- src/main/java/uk/gov/cslearning/catalogue/service/upload/processor/mp4/ParserFactory.java
+++ src/main/java/uk/gov/cslearning/catalogue/service/upload/processor/mp4/ParserFactory.java
@@ -1,5 +1,6 @@
 package uk.gov.cslearning.catalogue.service.upload.processor.mp4;
 
+import org.apache.tika.parser.AutoDetectParser;
 import org.apache.tika.parser.Parser;
 import org.apache.tika.parser.mp4.MP4Parser;
 import org.springframework.stereotype.Component;
@@ -8,5 +9,9 @@ import org.springframework.stereotype.Component;
 public class ParserFactory {
     public Parser createMp4Parser() {
         return new MP4Parser();
+    }
+
+    public Parser create() {
+        return new AutoDetectParser();
     }
 }
