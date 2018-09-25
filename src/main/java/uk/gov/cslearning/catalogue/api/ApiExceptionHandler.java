c7ef129b0e3843baa4c701f8753c4cbeca0850cd
Jakub Kozlowski <42406711+jakub-kainos@users.noreply.github.com>
2018-09-25 11:14:36 +0100

    LPFG-758: deleting audience (#40)
    * LPFG-758: deleting audience
    
    * LPFG-758: resoring removed Void generic ResponseEntities

--- src/main/java/uk/gov/cslearning/catalogue/api/ApiExceptionHandler.java
+++ src/main/java/uk/gov/cslearning/catalogue/api/ApiExceptionHandler.java
@@ -3,9 +3,11 @@ package uk.gov.cslearning.catalogue.api;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import uk.gov.cslearning.catalogue.exception.ResourceNotFoundException;
 import uk.gov.cslearning.catalogue.exception.UnknownFileTypeException;
 
 @ControllerAdvice
@@ -16,5 +18,10 @@ public class ApiExceptionHandler {
     @ExceptionHandler({IllegalStateException.class, UnknownFileTypeException.class})
     public void handleIllegalStateException(Exception e) {
         LOGGER.error("Bad Request: ", e);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return ResponseEntity.notFound().build();
     }
 }
