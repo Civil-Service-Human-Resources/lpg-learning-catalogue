d2844fc8af04ce7427aacf0f63aa0f7fea465600
Rob Leney <roble@kainos.com>
2018-10-18 12:58:40 +0100

    LPFG-889: Added Jackson annotations to Status enum

--- src/main/java/uk/gov/cslearning/catalogue/domain/Status.java
+++ src/main/java/uk/gov/cslearning/catalogue/domain/Status.java
@@ -1,7 +1,31 @@
 package uk.gov.cslearning.catalogue.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import java.util.Arrays;
+
 public enum Status {
-    DRAFT,
-    PUBLISHED,
-    ARCHIVED
+    DRAFT("Draft"),
+    PUBLISHED("Published"),
+    ARCHIVED("Archived");
+
+    private final String value;
+
+    Status(String value) {
+        this.value = value;
+    }
+
+    @JsonCreator
+    public static Status forValue(String value) {
+        return Arrays.stream(Status.values())
+                .filter(v -> v.value.equalsIgnoreCase(value))
+                .findAny()
+                .orElseThrow(() -> new RuntimeException("Uknown Status"));
+    }
+
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
 }
