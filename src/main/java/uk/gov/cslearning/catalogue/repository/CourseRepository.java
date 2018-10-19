d59c9dbd5a1ef260086b0dc3b7372b0ae224268d
Rob Leney <roble@kainos.com>
2018-10-19 11:51:40 +0100

    LPFG-888: Updated course controller and repository to search for status. Default status to public if none is provided.

--- src/main/java/uk/gov/cslearning/catalogue/repository/CourseRepository.java
+++ src/main/java/uk/gov/cslearning/catalogue/repository/CourseRepository.java
@@ -8,15 +8,18 @@ import org.springframework.stereotype.Repository;
 import uk.gov.cslearning.catalogue.api.FilterParameters;
 import uk.gov.cslearning.catalogue.domain.Course;
 import uk.gov.cslearning.catalogue.domain.SearchPage;
+import uk.gov.cslearning.catalogue.domain.Status;
 
 @Repository
 public interface CourseRepository extends ElasticsearchRepository<Course, String>, ResourceSearchRepository{
 
-    @Query("{ \"bool\": { \"must\": [{ \"match\": { \"audiences.type\": \"REQUIRED_LEARNING\" } }, { \"term\": { \"audiences.departments\": \"?0\" }}] }}")
+    Page<Course> findAllByStatus(Status status, Pageable pageable);
+
+    @Query("{ \"bool\": { \"must\": [{ \"match\": { \"modules.audiences.mandatory\": \"true\" } }, { \"term\": { \"modules.audiences.departments\": \"?0\" }}] }}")
     Page<Course> findMandatory(String department, Pageable pageable);
 
-    @Query("{\"bool\": {\"should\": [{\"match\": {\"audiences.departments\": {\"query\": \"?0\",\"zero_terms_query\": \"none\"}}},{\"match\": {\"audiences.areasOfWork\": {\"query\": \"?1\",\"zero_terms_query\": \"none\"}}},{\"match\": {\"audiences.interests\": {\"query\": \"?2\",\"zero_terms_query\": \"none\"}}}],\"must_not\": [{\"match\": {\"audiences.type\": \"REQUIRED_LEARNING\"}}]}}")
-    Page<Course> findSuggested(String department, String areaOfWork, String interest, Pageable pageable);
+    @Query("{\"bool\": {\"should\": [{\"match\": {\"audiences.departments\": {\"query\": \"?0\",\"zero_terms_query\": \"none\"}}},{\"match\": {\"audiences.areasOfWork\": {\"query\": \"?1\",\"zero_terms_query\": \"none\"}}},{\"match\": {\"audiences.interests\": {\"query\": \"?2\",\"zero_terms_query\": \"none\"}}}],\"must\": [{\"match\": {\"status\": {\"query\": \"?3\"}}}],\"must_not\": [{\"match\": {\"audiences.type\": \"REQUIRED_LEARNING\"}}]}}")
+    Page<Course> findSuggested(String department, String areaOfWork, String interest, String status, Pageable pageable);
 
     SearchPage search(String query, Pageable pageable, FilterParameters filterParameters);
 }
