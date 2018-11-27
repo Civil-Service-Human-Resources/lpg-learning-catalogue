2ae98f57e8a0abed0f1649e25bdebfd435f7ad16
Peter Taylor <petertaylor@Malekith.local>
2018-11-27 11:47:06 +0000

    LPFG-979: Removed PO workaround

--- src/main/java/uk/gov/cslearning/catalogue/api/PurchaseOrderController.java
+++ src/main/java/uk/gov/cslearning/catalogue/api/PurchaseOrderController.java
@@ -53,16 +53,14 @@ public class PurchaseOrderController {
                                               @RequestParam("moduleId") String moduleId) {
         LOGGER.debug("Finding purchaseOrder for department {} and moduleId {}", department, moduleId);
 
-//        Iterable<PurchaseOrder> result = purchaseOrderRepository
-////                .findByDepartmentAndModulesContains(department, moduleId);
-////
-////        return StreamSupport.stream(result.spliterator(), false)
-////                .filter(purchaseOrder -> purchaseOrder.isValidFor(LocalDate.now()))
-////                .findFirst()
-////                .map(purchaseOrder -> new ResponseEntity<>(purchaseOrder, HttpStatus.OK))
-////                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        Iterable<PurchaseOrder> result = purchaseOrderRepository
+                .findByDepartmentAndModulesContains(department, moduleId);
 
-        return new ResponseEntity<PurchaseOrder>(new PurchaseOrder(), HttpStatus.OK);
+        return StreamSupport.stream(result.spliterator(), false)
+                .filter(purchaseOrder -> purchaseOrder.isValidFor(LocalDate.now()))
+                .findFirst()
+                .map(purchaseOrder -> new ResponseEntity<>(purchaseOrder, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @GetMapping("/{id}")
