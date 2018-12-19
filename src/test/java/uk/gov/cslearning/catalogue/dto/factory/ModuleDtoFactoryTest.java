397d301fd48571aac8e36c0ec66a5cb3b6247527
Rob Leney <roble@kainos.com>
2018-12-19 09:32:02 +0000

    LPFG-509: Added tests for required modules

--- src/test/java/uk/gov/cslearning/catalogue/dto/factory/ModuleDtoFactoryTest.java
+++ src/test/java/uk/gov/cslearning/catalogue/dto/factory/ModuleDtoFactoryTest.java
@@ -4,14 +4,13 @@ import org.junit.Test;
 import uk.gov.cslearning.catalogue.domain.module.FaceToFaceModule;
 import uk.gov.cslearning.catalogue.dto.ModuleDto;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class ModuleDtoFactoryTest {
     private ModuleDtoFactory dtoFactory = new ModuleDtoFactory();
 
     @Test
-    public void shouldReturnModuleDto() {
+    public void shouldReturnRequiredModuleDto() {
         String id = "module-id";
         String title = "module-title";
 
@@ -24,5 +23,21 @@ public class ModuleDtoFactoryTest {
         assertEquals(id, dto.getId());
         assertEquals(title, dto.getTitle());
         assertTrue(dto.isRequired());
+    }
+
+    @Test
+    public void shouldReturnModuleDto() {
+        String id = "module-id";
+        String title = "module-title";
+
+        FaceToFaceModule module = new FaceToFaceModule("abc");
+        module.setOptional(true);
+        module.setId(id);
+        module.setTitle(title);
+
+        ModuleDto dto = dtoFactory.create(module);
+        assertEquals(id, dto.getId());
+        assertEquals(title, dto.getTitle());
+        assertFalse(dto.isRequired());
     }
 }
