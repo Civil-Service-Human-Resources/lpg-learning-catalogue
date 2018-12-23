6f328691cb0de0c075ecc021af7a24bd6e797082
Rob Leney <roble@kainos.com>
2018-12-23 20:07:32 +0000

    LPFG-290: Optimised imports

--- src/main/java/uk/gov/cslearning/catalogue/service/record/LearnerRecordService.java
+++ src/main/java/uk/gov/cslearning/catalogue/service/record/LearnerRecordService.java
@@ -2,13 +2,13 @@ package uk.gov.cslearning.catalogue.service.record;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.beans.factory.annotation.Value;
 import uk.gov.cslearning.catalogue.domain.module.CancellationReason;
 import uk.gov.cslearning.catalogue.domain.module.EventStatus;
 import uk.gov.cslearning.catalogue.service.IdentityTokenServices;
