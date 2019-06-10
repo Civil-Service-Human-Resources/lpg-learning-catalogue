6a2bd616e1bff0c03259ba02502ab86fbd43f4cb
matthewmu <matthewmu@kainos.com>
2019-06-10 16:13:39 +0100

    LC-217: updating elastic config to set readtimeout from 3s to 10s

--- src/main/java/uk/gov/cslearning/catalogue/config/ElasticSearchProperties.java
+++ src/main/java/uk/gov/cslearning/catalogue/config/ElasticSearchProperties.java
@@ -1,10 +1,12 @@
 package uk.gov.cslearning.catalogue.config;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.net.URI;
 
+@Data
 @Component
 @ConfigurationProperties(prefix = "elasticsearch")
 public class ElasticSearchProperties {
@@ -15,27 +17,5 @@ public class ElasticSearchProperties {
 
     private String password;
 
-    public URI getUri() {
-        return uri;
-    }
-
-    public void setUri(URI uri) {
-        this.uri = uri;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    private int readTimeout;
 }
