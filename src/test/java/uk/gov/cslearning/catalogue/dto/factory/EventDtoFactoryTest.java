1a804b347b515d93f4a537f3721022d25b8d0760
Rewaz <rewaz96@hotmail.com>
2020-02-11 14:40:52 +0000

    added location, event date, booking ID and event ID to the MI report

--- src/test/java/uk/gov/cslearning/catalogue/dto/factory/EventDtoFactoryTest.java
+++ src/test/java/uk/gov/cslearning/catalogue/dto/factory/EventDtoFactoryTest.java
@@ -7,12 +7,17 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import uk.gov.cslearning.catalogue.domain.Course;
 import uk.gov.cslearning.catalogue.domain.LearningProvider;
+import uk.gov.cslearning.catalogue.domain.module.DateRange;
 import uk.gov.cslearning.catalogue.domain.module.Event;
 import uk.gov.cslearning.catalogue.domain.module.FaceToFaceModule;
+import uk.gov.cslearning.catalogue.domain.module.Venue;
 import uk.gov.cslearning.catalogue.dto.CourseDto;
 import uk.gov.cslearning.catalogue.dto.EventDto;
 import uk.gov.cslearning.catalogue.dto.LearningProviderDto;
 import uk.gov.cslearning.catalogue.dto.ModuleDto;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.verifyZeroInteractions;
@@ -36,12 +41,16 @@ public class EventDtoFactoryTest {
         FaceToFaceModule module = new FaceToFaceModule("product-code");
         Course course = new Course();
         LearningProvider learningProvider = new LearningProvider();
+        Venue venue = new Venue();
+        List<DateRange> dateRanges = new ArrayList<>();
 
         ModuleDto moduleDto = new ModuleDto();
         CourseDto courseDto = new CourseDto();
         LearningProviderDto learningProviderDto = new LearningProviderDto();
 
         course.setLearningProvider(learningProvider);
+        event.setDateRanges(dateRanges);
+        event.setVenue(venue);
 
         when(moduleDtoFactory.create(module, course)).thenReturn(moduleDto);
         when(learningProviderDtoFactory.create(learningProvider)).thenReturn(learningProviderDto);
@@ -52,6 +61,8 @@ public class EventDtoFactoryTest {
         assertEquals(learningProviderDto, eventDto.getLearningProvider());
         assertNotNull(eventDto.getId());
         assertEquals(event.getId(), eventDto.getId());
+        assertEquals(event.getVenue().getLocation(), eventDto.getLocation());
+        assertEquals(eventDtoFactory.getEventDatesFromDateRanges(event.getDateRanges()), eventDto.getEventDate());
     }
 
     @Test
@@ -59,17 +70,25 @@ public class EventDtoFactoryTest {
         Event event = new Event();
         FaceToFaceModule module = new FaceToFaceModule("product-code");
         Course course = new Course();
+        Venue venue = new Venue();
+        List<DateRange> dateRanges = new ArrayList<>();
+
+        event.setDateRanges(dateRanges);
+        event.setVenue(venue);
 
         ModuleDto moduleDto = new ModuleDto();
 
-        when(moduleDtoFactory.create(module,course)).thenReturn(moduleDto);
+        when(moduleDtoFactory.create(module, course)).thenReturn(moduleDto);
 
         EventDto eventDto = eventDtoFactory.create(event, module, course);
+
 
         assertEquals(moduleDto, eventDto.getModule());
         assertNull(eventDto.getLearningProvider());
         assertNotNull(eventDto.getId());
         assertEquals(event.getId(), eventDto.getId());
+        assertEquals(event.getVenue().getLocation(), eventDto.getLocation());
+        assertEquals(eventDtoFactory.getEventDatesFromDateRanges(event.getDateRanges()), eventDto.getEventDate());
 
         verifyZeroInteractions(learningProviderDtoFactory);
     }
