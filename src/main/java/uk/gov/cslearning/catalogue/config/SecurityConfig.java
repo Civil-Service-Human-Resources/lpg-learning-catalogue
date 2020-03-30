101bf6a3be70dfcacae1008cd0e172a7e56218c0
Marcin Mackiewicz <m.mackiewicz@kainos.com>
2020-03-30 17:54:27 +0200

    Implemented PoolingHttpClientConnectionManager to be used for requests (#118)
    * Implemented PoolingHttpClientConnectionManager to be used for requests
    
    * Reduced max connections

--- src/main/java/uk/gov/cslearning/catalogue/config/SecurityConfig.java
+++ src/main/java/uk/gov/cslearning/catalogue/config/SecurityConfig.java
@@ -1,8 +1,15 @@
 package uk.gov.cslearning.catalogue.config;
 
+import org.apache.http.HttpHost;
+import org.apache.http.conn.routing.HttpRoute;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -51,8 +58,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public OAuth2RestOperations oAuthRestTemplate(OAuth2ProtectedResourceDetails resourceDetails) {
+    public PoolingHttpClientConnectionManager httpClientConnectionManager(OAuthProperties oAuthProperties) {
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(oAuthProperties.getMaxTotalConnections());
+        connectionManager.setDefaultMaxPerRoute(oAuthProperties.getDefaultMaxConnectionsPerRoute());
+        HttpHost host = new HttpHost(oAuthProperties.getServiceUrl());
+        connectionManager.setMaxPerRoute(new HttpRoute(host), oAuthProperties.getMaxPerServiceUrl());
+
+        return connectionManager;
+    }
+
+    @Bean
+    public OAuth2RestOperations oAuthRestTemplate(OAuth2ProtectedResourceDetails resourceDetails, PoolingHttpClientConnectionManager connectionManager) {
+        CloseableHttpClient httpClient = HttpClients.custom()
+            .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
+            .setConnectionManager(connectionManager)
+            .build();
+
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setHttpClient(httpClient);
+
         AccessTokenRequest atr = new DefaultAccessTokenRequest();
-        return new OAuth2RestTemplate(resourceDetails, new DefaultOAuth2ClientContext(atr));
+        OAuth2RestTemplate oAuthRestTemplate = new OAuth2RestTemplate(resourceDetails, new DefaultOAuth2ClientContext(atr));
+        oAuthRestTemplate.setRequestFactory(requestFactory);
+
+        return oAuthRestTemplate;
     }
 }
