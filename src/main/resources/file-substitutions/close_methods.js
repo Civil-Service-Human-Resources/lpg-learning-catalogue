595d65e70abef157f3dd73a8bed5d28da9cffb95
Jain Neeraj <neeraj.jain@cabinetoffice.gov.uk>
2021-05-20 13:12:10 +0100

    LC-939 javascript files are updated for new test URLs

--- src/main/resources/file-substitutions/close_methods.js
+++ src/main/resources/file-substitutions/close_methods.js
@@ -7,7 +7,12 @@ var CLOSE_METHODS = {
 
             var url = window.location.toString();
             var prodMatch = url.match(/(https?):\/\/([^-]*)-?cdn\.learn\.civilservice\.gov\.uk\/[^/]+\/([^/]+)\/([^/]+)\/.*$/);
-            var nonProdMatch = url.match(/(https?):\/\/([^-]*)-?cdn\.cshr\.digital\/[^/]+\/([^/]+)\/([^/]+)\/.*$/);
+            //var nonProdMatch = url.match(/(https?):\/\/([^-]*)-?cdn\.cshr\.digital\/[^/]+\/([^/]+)\/([^/]+)\/.*$/);
+            var nonProdMatch = url.match(/(https?):\/\/([^-]*)-?cdn\.*\.learn\.civilservice\.gov\.uk\/[^/]+\/([^/]+)\/([^/]+)\/.*$/);
+
+            console.log('url: ' + url);
+            console.log('prodMatch: ' + prodMatch);
+            console.log('nonProdMatch: ' + nonProdMatch);
 
             if (!prodMatch && !nonProdMatch) {
                 console.log('Content being accessed on invalid domain');
@@ -17,22 +22,39 @@ var CLOSE_METHODS = {
             var courseId;
             var host;
             if (prodMatch) {
+                console.log('prodMatch[3]: ' + prodMatch[3]);
                 courseId = prodMatch[3];
                 host = 'learn.civilservice.gov.uk/';
             } else {
+                console.log('nonProdMatch[1]: ' + nonProdMatch[1]);
+                console.log('nonProdMatch[2]: ' + nonProdMatch[2]);
+                console.log('nonProdMatch[3]: ' + nonProdMatch[3]);
+                console.log('nonProdMatch[4]: ' + nonProdMatch[4]);
                 courseId = nonProdMatch[3];
+                console.log('courseId = nonProdMatch[3]: ' + courseId);
                 if (nonProdMatch[2] === 'local') {
                     host = 'lpg.local.cshr.digital:3001/';
                 } else {
-                    host = nonProdMatch[2] + '-lpg.cshr.digital/';
+                    host = nonProdMatch[2] + '.learn.civilservice.gov.uk/';
+                    console.log('host = nonProdMatch[2] + .learn.civilservice.gov.uk: ' + host);
                 }
             }
+            console.log('host: ' + host);
 
             var scheme = window.location.protocol;
+            console.log('scheme: ' + scheme);
+
             var moduleId = getParameterByName('module');
+            console.log('moduleId: ' + courseId);
+
             var path = 'learning-record/' + courseId + '/' + moduleId;
+            console.log('path = learning-record/ + courseId + / + moduleId: ' + path);
 
             window.location = scheme + '//' + host + path;
+            console.log('window.location = scheme + // + host + path: ' + scheme + '//' + host + path);
+            console.log('window.location: ' + window.location);
+
+            alert('close_methods.js');
 
             return true;
     	}
--- src/main/resources/file-substitutions/portal_overrides.js
+++ src/main/resources/file-substitutions/portal_overrides.js
@@ -12,7 +12,12 @@ console.log('portal_overrides.js');
 
 var url = window.location.toString();
 var prodMatch = url.match(/(https?):\/\/([^-]*)-?cdn\.learn\.civilservice\.gov\.uk\/[^/]+\/([^/]+)\/([^/]+)\/.*$/);
-var nonProdMatch = url.match(/(https?):\/\/([^-]*)-?cdn\.cshr\.digital\/[^/]+\/([^/]+)\/([^/]+)\/.*$/);
+//var nonProdMatch = url.match(/(https?):\/\/([^-]*)-?cdn\.cshr\.digital\/[^/]+\/([^/]+)\/([^/]+)\/.*$/);
+var nonProdMatch = url.match(/(https?):\/\/([^-]*)-?cdn\.*\.learn\.civilservice\.gov\.uk\/[^/]+\/([^/]+)\/([^/]+)\/.*$/);
+
+console.log('url: ' + url);
+console.log('prodMatch: ' + prodMatch);
+console.log('nonProdMatch: ' + nonProdMatch);
 
 if (!prodMatch && !nonProdMatch) {
     console.log('Content being accessed on invalid domain');
@@ -22,20 +27,37 @@ if (!prodMatch && !nonProdMatch) {
 var courseId;
 var host;
 if (prodMatch) {
+    console.log('prodMatch[3]: ' + prodMatch[3]);
     courseId = prodMatch[3];
     host = 'learn.civilservice.gov.uk/';
 } else {
+    console.log('nonProdMatch[1]: ' + nonProdMatch[1]);
+    console.log('nonProdMatch[2]: ' + nonProdMatch[2]);
+    console.log('nonProdMatch[3]: ' + nonProdMatch[3]);
+    console.log('nonProdMatch[4]: ' + nonProdMatch[4]);
     courseId = nonProdMatch[3];
+    console.log('courseId = nonProdMatch[3]: ' + courseId);
     if (nonProdMatch[2] === 'local') {
         host = 'lpg.local.cshr.digital:3001/';
     } else {
-        host = nonProdMatch[2] + '-lpg.cshr.digital/';
+        host = nonProdMatch[2] + '.learn.civilservice.gov.uk/';
+        console.log('host = nonProdMatch[2] + .learn.civilservice.gov.uk: ' + host);
     }
 }
+console.log('host: ' + host);
 
 var scheme = window.location.protocol;
+console.log('scheme: ' + scheme);
+
 var moduleId = getParameterByName('module');
+console.log('moduleId: ' + courseId);
+
 var path = 'learning-record/' + courseId + '/' + moduleId + '/xapi';
+console.log('path = learning-record/ + courseId + / + moduleId + /xapi: ' + path);
+
+console.log('endpoint = scheme + // + host + path: ' + scheme + '//' + host + path);
+
+alert('portal_overrides.js');
 
 BOO_INCLUDE_EXIT_ON_NAV = false;
 BOO_INCLUDE_ACCESSIBLE_ON_NAV = false;
