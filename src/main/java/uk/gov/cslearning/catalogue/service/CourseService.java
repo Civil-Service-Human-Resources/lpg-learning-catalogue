8f701c4d33b2724a14682bac1e367fc7b8a8126c
Jain Neeraj <neeraj.jain@cabinetoffice.gov.uk>
2021-09-24 11:49:20 +0100

    LC-1072 course disappear on home page refresh logic improvement

--- src/main/java/uk/gov/cslearning/catalogue/service/CourseService.java
+++ src/main/java/uk/gov/cslearning/catalogue/service/CourseService.java
@@ -5,7 +5,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
-
 import java.util.*;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
