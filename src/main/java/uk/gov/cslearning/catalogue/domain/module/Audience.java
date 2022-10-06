565e6a9398686c94ebd811848e4fc71d9f0b0fc5
jaylco <77386599+jaylco@users.noreply.github.com>
2022-10-06 14:10:57 +0100

    URL conversion

--- src/main/java/uk/gov/cslearning/catalogue/domain/module/Audience.java
+++ src/main/java/uk/gov/cslearning/catalogue/domain/module/Audience.java
@@ -2,10 +2,13 @@ package uk.gov.cslearning.catalogue.domain.module;
 
 import lombok.Data;
 import org.elasticsearch.common.UUIDs;
+import org.springframework.data.elasticsearch.annotations.Field;
 
 import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
+
+import static org.springframework.data.elasticsearch.annotations.FieldType.Date;
 
 @Data
 public class Audience {
@@ -28,8 +31,10 @@ public class Audience {
 
     private Set<String> interests = new HashSet<>();
 
+
     private Instant requiredBy;
 
+    @Field(type = Date, format = {}, pattern = "uuuu-MM-dd'T'HH:mm:ss")
     private String frequency;
 
     private Type type;
