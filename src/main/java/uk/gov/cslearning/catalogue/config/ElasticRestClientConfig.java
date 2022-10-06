5ccc4414dcdd934b701808d4780d4a64bf2201fb
jaylco <77386599+jaylco@users.noreply.github.com>
2022-10-06 11:29:43 +0100

    upgraded springboot and added ES compatibility mode

--- src/main/java/uk/gov/cslearning/catalogue/config/ElasticRestClientConfig.java
+++ src/main/java/uk/gov/cslearning/catalogue/config/ElasticRestClientConfig.java
@@ -1,11 +1,13 @@
 package uk.gov.cslearning.catalogue.config;
 
+import org.apache.http.Header;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.message.BasicHeader;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -51,9 +53,18 @@ public class ElasticRestClientConfig extends AbstractElasticsearchConfiguration 
         final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
         credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
         RestClientBuilder restClientBuilder = RestClient.builder(new HttpHost(host, port, protocol));
-        restClientBuilder.setHttpClientConfigCallback(builder -> builder.setDefaultCredentialsProvider(credentialsProvider))
+        restClientBuilder
+                .setHttpClientConfigCallback(builder -> builder.setDefaultCredentialsProvider(credentialsProvider))
+                .setDefaultHeaders(compatibilityHeaders())
                 .setRequestConfigCallback(requestConfigBuilder -> requestConfigBuilder.setSocketTimeout(readTimeout));
         return new RestHighLevelClient(restClientBuilder);
 
+    }
+
+    private Header[] compatibilityHeaders() {
+        return new Header[]{
+                new BasicHeader(HttpHeaders.ACCEPT, "application/vnd.elasticsearch+json;compatible-with=7"),
+                new BasicHeader(HttpHeaders.CONTENT_TYPE, "application/vnd.elasticsearch+json;compatible-with=7")
+        };
     }
 }
