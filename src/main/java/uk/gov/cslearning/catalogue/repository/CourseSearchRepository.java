9bf36fcbcc2a7a97156baedda3b7b3b81b74d342
SavvasCO <savvas.stephanides@cabinetoffice.gov.uk>
2023-10-25 10:33:21 +0100

    Search filter bug fix

--- src/main/java/uk/gov/cslearning/catalogue/repository/CourseSearchRepository.java
+++ src/main/java/uk/gov/cslearning/catalogue/repository/CourseSearchRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Pageable;
 import uk.gov.cslearning.catalogue.api.FilterParameters;
 import uk.gov.cslearning.catalogue.api.OwnerParameters;
 import uk.gov.cslearning.catalogue.api.ProfileParameters;
+import uk.gov.cslearning.catalogue.api.v2.model.CourseSearchParameters;
 import uk.gov.cslearning.catalogue.domain.Course;
 import uk.gov.cslearning.catalogue.domain.SearchPage;
 import uk.gov.cslearning.catalogue.domain.Status;
@@ -13,6 +14,8 @@ import java.util.Collection;
 
 public interface CourseSearchRepository {
     SearchPage search(String query, Pageable pageable, FilterParameters filterParameters, Collection<Status> status, OwnerParameters ownerParameters, ProfileParameters profileParameters, String visbility);
+
+    Page<Course> search(CourseSearchParameters parameters, Pageable pageable);
 
     Page<Course> findAllByOrganisationCode(String organisationalUnitCode, Pageable pageable);
 
---
+++ src/test/java/uk/gov/cslearning/catalogue/domain/CourseSearchParametersTest.java
@@ -0,0 +1,58 @@
+package uk.gov.cslearning.catalogue.domain;
+
+import org.junit.Test;
+import uk.gov.cslearning.catalogue.api.v2.model.CourseSearchParameters;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class CourseSearchParametersTest {
+    @Test
+    public void testHasAudienceFieldReturnsFalseIfNoneOfAudienceFieldsIsPopulated() {
+        boolean expectedResult = false;
+        CourseSearchParameters parameters = new CourseSearchParameters();
+        parameters.setSearchTerm("corruption");
+
+        boolean actualResult = parameters.hasAudienceFields();
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testHasAudienceFieldReturnsTrueIfDepartmentsIsPopulated() {
+        boolean expectedResult = true;
+        CourseSearchParameters parameters = new CourseSearchParameters();
+        parameters.setSearchTerm("corruption");
+        List<String> departments = new ArrayList<>();
+        departments.add("co");
+        parameters.setDepartments(departments);
+
+        boolean actualResult = parameters.hasAudienceFields();
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testCostIsFreeReturnsTrueIfCostStringIsFree() {
+        boolean expectedResult = true;
+        CourseSearchParameters parameters = new CourseSearchParameters();
+        parameters.setCost("free");
+
+        boolean actualResult = parameters.costIsFree();
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testCostIsFreeReturnsFalseIfCostNotSpecified() {
+        boolean expectedResult = false;
+        CourseSearchParameters parameters = new CourseSearchParameters();
+
+        boolean actualResult = parameters.costIsFree();
+
+        assertEquals(expectedResult, actualResult);
+    }
+}
