afa52fa0162820d04c4fba4be5ba8720adcc38df
Jamie <jamie.lovell@cabinetoffice.gov.uk>
2025-02-17 10:43:22 +0000

    Make elastic statically initialised again

--- src/test/java/uk/gov/cslearning/catalogue/integration/IntegrationTestBase.java
+++ src/test/java/uk/gov/cslearning/catalogue/integration/IntegrationTestBase.java
@@ -1,6 +1,11 @@
 package uk.gov.cslearning.catalogue.integration;
 
 
+import com.github.dockerjava.api.model.ExposedPort;
+import com.github.dockerjava.api.model.HostConfig;
+import com.github.dockerjava.api.model.PortBinding;
+import com.github.dockerjava.api.model.Ports;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.runner.RunWith;
@@ -13,20 +18,24 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.ResultActions;
-import uk.gov.cslearning.catalogue.util.DataService;
+import org.testcontainers.elasticsearch.ElasticsearchContainer;
+import org.testcontainers.utility.DockerImageName;
 import uk.gov.cslearning.catalogue.config.DisableBlobStorage;
-import uk.gov.cslearning.catalogue.config.ElasticContainer;
 import uk.gov.cslearning.catalogue.config.IntegrationTestConfig;
+import uk.gov.cslearning.catalogue.util.DataService;
 
 import javax.annotation.PostConstruct;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 
 @ActiveProfiles("integration-test")
 @RunWith(SpringRunner.class)
-@Import({IntegrationTestConfig.class, ElasticContainer.class, DisableBlobStorage.class})
+@Import({IntegrationTestConfig.class, DisableBlobStorage.class})
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
         properties = {"spring.main.allow-bean-definition-overriding=true"})
 @AutoConfigureMockMvc
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Slf4j
 public abstract class IntegrationTestBase {
 
     @Autowired
@@ -35,7 +44,27 @@ public abstract class IntegrationTestBase {
     @Autowired
     protected DataService dataService;
 
-    protected boolean coursesLoaded = false;
+    protected static String elasticImage = "docker.elastic.co/elasticsearch/elasticsearch:8.4.2";
+
+    static ElasticsearchContainer ELASTICSEARCH_CONTAINER;
+    static {
+        boolean useLocalElastic = Boolean.parseBoolean(System.getenv("INTEGRATION_TEST_USE_LOCAL_ELASTIC"));
+        if (!useLocalElastic) {
+            log.info("useLocalElastic setting is false. Creating an elasticsearch container.");
+            ELASTICSEARCH_CONTAINER = new ElasticsearchContainer(DockerImageName.parse(elasticImage));
+            ELASTICSEARCH_CONTAINER.withEnv("discovery.type", "single-node");
+            ELASTICSEARCH_CONTAINER.withEnv("cluster.name", "local");
+            ELASTICSEARCH_CONTAINER.withEnv("xpack.security.enabled", "false");
+            ELASTICSEARCH_CONTAINER.withExposedPorts(9200);
+            ELASTICSEARCH_CONTAINER.withCreateContainerCmdModifier(cmd -> cmd.withHostConfig(
+                    new HostConfig().withPortBindings(new PortBinding(Ports.Binding.bindPort(9200), new ExposedPort(9200)))
+            ));
+            ELASTICSEARCH_CONTAINER.withStartupTimeout(Duration.of(5, ChronoUnit.MINUTES));
+            ELASTICSEARCH_CONTAINER.start();
+        } else {
+            log.info("useLocalElastic setting is true. Using a local elasticsearch instance on port 9200.");
+        }
+    }
 
     @Before
     public void beforeAll() {
